263c7d807e6f801578b5d54ab1f9d27fccb42997
Gaston Gomez <gaston.gomez.1998@gmail.com>
2024-04-24 14:00:33 -0300

    Se agrego codigo para traer los comentarios y las publicaciones

--- script.js
+++ script.js
@@ -35,14 +35,41 @@ let viviendas = [
     }
 ]
 
+let comentarios = [
+    {
+        autor: "Juan Perez",
+        comentario: "El personal se comporto de una excelente forma, muy buena atencion y la casa que me ofrecieron cumple con todas mis expectativas."
+    }
+]
+
+let blogs = [
+    {
+        titulo: "Terraza en el techo de tu casa",
+        img: "images/blog1.jpg",
+        fecha: "20/10/2019",
+        autor: "Admin",
+        descripcion: "Consejos para construir una terraza en el techo de tu casa, con los mejores materiales y ahorrando dinero"
+    },
+    {
+        titulo: "Guía para la decoración de tu hogar",
+        img: "images/blog2.jpg",
+        fecha: "20/10/2019",
+        autor: "Admin",
+        descripcion: "Maximiza el espacio de tu hogar con esta guia, aprende a combinar muebles y colores para darle vida a tu espacio" 
+    }
+]
+
 function onInit(){
     console.log("Entro al init");
     cargarPropiedades(viviendas);
+    cargarComentarios(comentarios);
+    cargarBlogs(blogs)
+    cargarFooter();
 }
 
 function cargarPropiedades(propiedades){
+    let sectionCasas = document.getElementById("anuncios")
     propiedades.forEach(element => {
-        let sectionCasas = document.getElementById("anuncios")
         let casa = document.createElement("div");
         let foto = document.createElement("img");
         let nombre = document.createElement("h3");
@@ -60,6 +87,7 @@ function cargarPropiedades(propiedades){
         let detDorm = document.createElement("div");
         let iconDorm = document.createElement("img");
         let cantDorm = document.createElement("p");
+
         detalles.className = 'casa-detalles'
         casa.className = 'casa';
         casa.id = element.id;
@@ -72,23 +100,24 @@ function cargarPropiedades(propiedades){
         detBanios.classList= "cuartos";
         iconBanios.src = "./images/icono_wc.svg";
         cantBanios.textContent = element.banios;
-        cuartos.appendChild(detBanios);
-        detBanios.appendChild(iconBanios);
-        detBanios.appendChild(cantBanios);
         detGarage.classList= "cuartos";
         iconGarage.src = "./images/icono_estacionamiento.svg";
         cantGarage.textContent = element.garages;
-        cuartos.appendChild(detGarage);
-        detGarage.appendChild(iconGarage);
-        detGarage.appendChild(cantGarage);
         detDorm.classList= "cuartos"
         iconDorm.src = "./images/icono_dormitorio.svg";
         cantDorm.textContent = element.habitaciones;
+        verPropiedad.textContent = "Ver Propiedad";
+        verPropiedad.href="";
+
+        cuartos.appendChild(detBanios);
+        detBanios.appendChild(iconBanios);
+        detBanios.appendChild(cantBanios);
+        cuartos.appendChild(detGarage);
+        detGarage.appendChild(iconGarage);
+        detGarage.appendChild(cantGarage);
         cuartos.appendChild(detDorm);
         detDorm.appendChild(iconDorm);
         detDorm.appendChild(cantDorm);
-        verPropiedad.textContent = "Ver Propiedad";
-        verPropiedad.href="";
         casa.appendChild(foto);
         detalles.appendChild(nombre);
         detalles.appendChild(descripcion);
@@ -98,4 +127,73 @@ function cargarPropiedades(propiedades){
         casa.appendChild(detalles);
         sectionCasas.appendChild(casa);
     });
+}
+
+function cargarComentarios(lista){
+    let sectionComentarios = document.getElementById("comentarios")
+    lista.forEach(element => {
+        let comentario = document.createElement('div');
+        let comText = document.createElement('p');
+        let comAutor = document.createElement('p');
+        let comilla = document.createElement('img')
+
+        comentario.classList = 'comentario';
+        comilla.src = './images/comilla.svg';
+        comText.id = 'com-text'
+        comAutor.id = 'com-aut'
+        comText.textContent = element.comentario;
+        comAutor.textContent = `-${element.autor}`;
+        sectionComentarios.appendChild(comentario);
+
+        comentario.appendChild(comilla);
+        comentario.appendChild(comText);
+        comentario.appendChild(comAutor);
+
+    });
+}
+
+function cargarBlogs(lista){
+    let sectionBlogs = document.getElementById('blog')
+    lista.forEach(element =>{
+        let blog = document.createElement('div');
+        let datos = document.createElement('div');
+        let imagen = document.createElement('img');
+        let titulo = document.createElement('h3');
+        let separador = document.createElement('div');
+        let autorYFecha = document.createElement('p');
+        let fecha = document.createElement('p');
+        let autor = document.createElement('p');
+        let descripcion = document.createElement('p');
+
+        blog.classList = 'blog';
+        datos.classList = 'datos-blog';
+        imagen.src = element.img;
+        titulo.textContent = element.titulo;
+        separador.classList = 'separador'
+        autorYFecha.classList = "fecha-y-autor";
+        fecha.textContent = element.fecha;
+        fecha.classList = "fecha-blog";
+        autor.textContent = element.autor;
+        autor.classList = "autor-blog";
+        autorYFecha.innerHTML = `Escrito el:<span style="color: #E08709; margin-left:2%; margin-right:2%;">${fecha.innerText}</span> por:<span style="color: #E08709;margin-left:2%; margin-right:2%;">${autor.innerText}</span>`;
+        descripcion.textContent = element.descripcion ;
+
+        sectionBlogs.appendChild(blog);
+        blog.appendChild(imagen);
+        blog.appendChild(datos);
+        datos.appendChild(titulo);
+        datos.appendChild(separador);
+        datos.appendChild(autorYFecha);
+        datos.appendChild(descripcion);
+    })
+}
+
+function getYear(){
+    let fecha = new Date();
+    return fecha.getFullYear();
+}
+
+function cargarFooter(){
+    let footer = document.getElementById("derechos");
+    footer.textContent += getYear()+' \u00A9';
 }
